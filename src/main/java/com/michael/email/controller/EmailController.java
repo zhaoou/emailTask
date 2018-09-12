756259f5b5d928b722261ea09cad400ef6859ebe
zhaoou <zhaoouzhao@gmail.com>
2018-09-12 17:07:47 -0600

    dev2018.9.12-1

--- src/main/java/com/michael/email/controller/EmailController.java
+++ src/main/java/com/michael/email/controller/EmailController.java
@@ -10,54 +10,70 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @Controller
 public class EmailController {
-	
-	@Autowired
-	EmailService emailService;
 
-	@Autowired
-	UserService userService;
-	
-	@GetMapping("/")
-	public String home(Model model) {
-		userService.creat(new User().setName("Michael"));
-		userService.creat(new User().setName("Sagid"));
-		model.addAttribute("users", userService.findAll());
-		return "login";//the name of html
-	}
+    @Autowired
+    EmailService emailService;
 
-	@GetMapping("/email/{id}")
-	public String findEmailsByUserId(@PathVariable("id") String id, Model model) {// userId
-		model.addAttribute("fromEmails", userService.findById(id).getSentEmails());
-		model.addAttribute("toEmails", emailService.findEmailsSentTo(userService.findById(id)));
-		////model.addAttribute("email", new Email(id,null,null));
+    @Autowired
+    UserService userService;
 
-		model.addAttribute("user", userService.findById(id));
-		model.addAttribute("email", new Email().setSentByUser(userService.findById(id)));
-		return "emails";//the name of html
-	}
-	
-	@PostMapping("/send")
-	public String sendEmail(Email email) {
-		//TODO email的id为什么过不来 ， form里怎么传多个对象，下拉框怎么放到form里传值
-		emailService.addEmailToUser(email);
-		emailService.creat(email);
-		return "redirect:/email/" + email.getSentByUser().getId() ;
-	}
-	
-	@GetMapping("/delete/{id}")
-	public String deleteEmail(@PathVariable("id") String id) {
-		Email email = emailService.findById(id);
-		emailService.deleteById(id);
-		email.getSentByUser().getSentEmails().remove(email);
-		return "redirect:/email/" + email.getSentByUser().getId() ;
-	}
+    @GetMapping("/")
+    public String home(Model model) {
+        if(userService.findAll().size()==0) {
+            userService.creat(new User().setName("Michael"));
+            userService.creat(new User().setName("Sagid"));
+        }
+        model.addAttribute("users", userService.findAll());
 
-	//@GetMapping("/edit/{id}")
-	@PostMapping("/edit")
-	public String changeEmail(Email email) {
-		emailService.creat(email);
-		return "redirect:/emails";
-	}
+        return "login";//the name of html
+    }
+
+
+   // @PostMapping("/email/{id}")
+    // public String findEmailsByUserId(@PathVariable("id") String id, Model model) {// userId
+
+    @PostMapping("/email")
+    public String findEmailsByUserId(@RequestParam(value="userId") String id, Model model) {// userId
+        System.out.println("userId is "+id);
+        model.addAttribute("fromEmails", userService.findById(id).getSentEmails());
+        model.addAttribute("toEmails", emailService.findEmailsSentTo(userService.findById(id)));
+        ////model.addAttribute("email", new Email(id,null,null));
+
+        model.addAttribute("user", userService.findById(id));
+        model.addAttribute("email", new Email().setSentByUser(userService.findById(id)));
+        List<User> users= userService.findAll();
+        users.remove(userService.findById(id));
+        model.addAttribute("users", users);
+        return "emails";//the name of html
+    }
+
+    //public String sendEmail( @ModelAttribute Email email, @RequestParam(value="userId") String userId ) {
+    @PostMapping("/send")
+    public String sendEmail( Model model, Email email) {
+        emailService.addEmailToUser(email);
+        emailService.creat(email);
+        //model.addAttribute("userId",email.getSentByUser().getId());
+        //return "redirect:/email/" + email.getSentByUser().getId() ;
+        //return "redirect:/email" ;
+        return "redirect:/" ;
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteEmail(@PathVariable("id") String id) {
+        Email email = emailService.findById(id);
+        emailService.deleteById(id);
+        email.getSentByUser().getSentEmails().remove(email);
+        return "redirect:/" ;
+    }
+
+//    //@GetMapping("/edit/{id}")
+//    @PostMapping("/edit")
+//    public String changeEmail(Email email) {
+//        emailService.creat(email);
+//        return "redirect:/email";
+//    }
 }
